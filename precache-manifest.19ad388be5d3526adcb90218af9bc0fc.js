81f2a3acac06339432506f934808b834472bfa63
dreampharos <dreampharos@gmail.com>
2020-03-18 16:16:51 +0900

    Updates

---
+++ precache-manifest.19ad388be5d3526adcb90218af9bc0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3508b40f0a0c8670c0a1c659b1255a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "2aa5a9317ab5ab636424",
+    "url": "/movie_app/static/css/main.980adc8c.chunk.css"
+  },
+  {
+    "revision": "b99d49699e0c589e8847",
+    "url": "/movie_app/static/js/2.9dac8e89.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.9dac8e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa5a9317ab5ab636424",
+    "url": "/movie_app/static/js/main.6cd5d1bd.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
